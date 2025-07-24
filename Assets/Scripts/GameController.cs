2b9f3f36084d49d2aa556ef3549b604ff0783da7
Gislainylopes <gislainy.lopes@escolar.ifrn.edu.br>
2025-07-24 12:05:06 -0300

    f

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject message, duck;
-
+    [SerializeField] private GameObject pipes, source;
+    private float interval= 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnPipes", 0f, interval);
+    }
+
+    private void SpawnPipes()
+    {
+        Instantiate(
+            pipes,
+            source.transform.position,
+            Quaternion.identity
+        );
     }
 
     // Update is called once per frame
--- Assets/Scripts/Pipes.cs
+++ Assets/Scripts/Pipes.cs
@@ -6,10 +6,13 @@ public class Pipes : MonoBehaviour
 {
 
     [SerializeField] private float speed;
+    [SerializeField] private float minY = -1f;
+    [SerializeField] private float maxY = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float altura = Random.Range(minY, maxY);
+        transform.position = new Vector2(transform.position.x, altura); 
     }
 
     // Update is called once per frame
@@ -17,7 +20,13 @@ public class Pipes : MonoBehaviour
     {
         transform.position = new Vector2(
             transform.position.x - speed * Time.deltaTime,
-            transform.position.x
+            transform.position.y
         );
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("DestroyPipe"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
