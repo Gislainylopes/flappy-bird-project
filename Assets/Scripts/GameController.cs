b815bd844ff2b0976398687fa8680d52eda055ee
Gislainylopes <gislainy.lopes@escolar.ifrn.edu.br>
2025-07-31 09:48:48 -0300

    corrigindo nome da variavel

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,7 +12,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        starded = false;
+        started = false;
         InvokeRepeating("SpawnPipes", 0f, interval);
     }
 
